bf369c4cde5f5389cd141c91e5ce5b4c08078fc4
doko <dokomaze>
2023-08-24 00:58:41 +0700

    dockerize mymikrotik

--- .config/entrypoint.sh
+++ .config/entrypoint.sh
@@ -8,10 +8,9 @@ sed -i "s+DOMAIN+$DOMAIN+" $HTDOCS/.setting
 fi
 
 # check database if empty
-mysql -h $DBHOST -u $DBUSER -p$DBPASS -sse "select count(*) from t_bandwith;" $DBNAME &> /dev/null
-STATUS=$?
-if [ ! $STATUS == "0" ];then
-	cat $HTDOCS/db/db_mikrotik_2022.sql | mysql -h $DBHOST -u $DBUSER -p$DBPASS $DBNAME
-fi
+while ! mysql -h $DBHOST -u $DBUSER -p$DBPASS -sse "select count(*) from t_bandwith;" $DBNAME  &> /dev/null ;do
+        cat $HTDOCS/db/db_mikrotik_2022.sql | mysql -h $DBHOST -u $DBUSER -p$DBPASS $DBNAME
+        sleep 2;
+done
 
 httpd -D FOREGROUND
--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,8 @@ ADD .config/entrypoint.sh /entrypoint.sh
 
 RUN apk update && apk add apache2 php8-apache2 php8-mysqli php8-session php8-ctype mariadb-client && \
     sed -i '/LoadModule rewrite_module/s/^#//g' /etc/apache2/httpd.conf && \
-    sed -i 's#AllowOverride [Nn]one#AllowOverride All#' /etc/apache2/httpd.conf && chmod +x /entrypoint.sh
+    sed -i 's#AllowOverride [Nn]one#AllowOverride All#' /etc/apache2/httpd.conf && \
+    sed -i 's/index.html/index.php/g' /etc/apache2/httpd.conf && chmod +x /entrypoint.sh
 
 ADD . ./
 
